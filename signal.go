8df91fc5294df902df0fb30d64a3206cd754a3f0
陶辰 <chen.tao@ifchange.com>
2020-04-15 16:06:44 +0800

    update

--- signal.go
+++ signal.go
@@ -1,132 +1,58 @@
-// 信号处理
-//kill -INT pid 终止
-//kill -TERM pid 重启
-//需要调用Wg.Add()
-//需要监听Shutdown通道
-package signal
+package main
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
-
 )
 
-type signalContext struct {
-	IsHTTP bool `json:"-"`
-	//Wg 业务方调用此变量注册工作
-	Wg *sync.WaitGroup `json:"-"`
-	//done 业务方调用Shutdowned函数获取所有任务已经退出的通知
-	done chan bool
+var Signal = make(chan int, 0)
+var ExitSignal int32
 
-	mu    *sync.Mutex
-	doing bool
-
-	//Shutdown 业务方手动监听此通道获知通知
-	Ctx    context.Context    `json:"-"`
-	Cancel context.CancelFunc `json:"-"`
+func Add()  {
+	atomic.AddInt32(&ExitSignal, 1)
 }
 
-var scx *signalContext
-
-func init() {
-	scx = &signalContext{
-		Wg:   new(sync.WaitGroup),
-		done: make(chan bool),
-		mu:   new(sync.Mutex),
-	}
-	scx.Ctx, scx.Cancel = context.WithCancel(context.Background())
+func Sub()  {
+	atomic.AddInt32(&ExitSignal, -1)
 }
 
-//GetSignalContext 一般用于其他包或者非http程序
-func GetSignalContext() *signalContext {
-	return scx
-}
+func main() {
 
-//gracehttp外，增加两个信号支持
-func (ctx *signalContext) Listen() {
+	go func() {
+		Add()
+		defer Sub()
+	forTag:
+		for {
+			select {
+			case <-Signal:
+				fmt.Println("End")
+				break forTag
+			}
+		}
+	}()
+
+	// init signal
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
-
-	log.Printf("Exec `kill -INT %d` will graceful exit", os.Getpid())
-	log.Printf("Exec `kill -TERM %d` will graceful restart", os.Getpid())
-
-	s := <-c
-	log.Printf("recv signal:%v", s)
-	go ctx.doShutdownDone()
-	if ctx.IsHTTP {
-		log.Println("Stopping http server")
-		//已有第三方处理
-	} else {
-		log.Println("Stopping console server")
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-c
+		log.Printf("discovery get a signal %s", s.String())
 		switch s {
-		case syscall.SIGTERM:
-			execSpec := &syscall.ProcAttr{
-				Env:   os.Environ(),
-				Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			close(Signal)
+			for ExitSignal > 0 {
+				time.Sleep(time.Second)
 			}
-			_, _, err := syscall.StartProcess(os.Args[0], os.Args, execSpec)
-			if err != nil {
-				log.Printf("failed to forkexec: %v", err)
-			}
-		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGINT:
+			log.Println("discovery quit !!!")
+			return
+		case syscall.SIGHUP:
+		default:
+			return
 		}
 	}
-}
-
-func (ctx *signalContext) doShutdownDone() {
-	ctx.mu.Lock()
-	defer ctx.mu.Unlock()
-	if ctx.doing {
-		return
-	}
-	ctx.doing = true
-
-	log.Println("doShutdownDone start.")
-	defer log.Println("doShutdownDone done.")
-
-	go ctx.waitDone()
-
-	timeout := 30
-	select {
-	case <-time.After(time.Duration(timeout) * time.Second):
-		log.Printf("doShutdownDone %ds timeout", timeout)
-		close(ctx.done)
-	case <-ctx.done:
-	}
-}
-
-//通知业务方，并等待业务方结束
-func (ctx *signalContext) waitDone() {
-	//context包来取消，以通知业务方
-	log.Println("signal ctx cancel")
-	ctx.Cancel()
-	//等待业务方完成退出
-	log.Println("signal ctx waitgroup wait done start")
-	ctx.WgWait()
-	//表示全部完成
-	log.Println("signal ctx waitgroup wait done end")
-	close(ctx.done)
-}
-
-//Shutdowned 获取是否已经全部结束，暂时只有run.go里用到
-func (ctx *signalContext) Shutdowned() {
-	go ctx.doShutdownDone()
-	<-ctx.done
-}
-
-func (ctx *signalContext) WgAdd() {
-	ctx.Wg.Add(1)
-}
-
-func (ctx *signalContext) WgDone() {
-	ctx.Wg.Done()
-}
-
-func (ctx *signalContext) WgWait() {
-	ctx.Wg.Wait()
 }
